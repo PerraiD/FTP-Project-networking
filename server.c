1132f7344035419f75a6abd15e4ab328aa50d0a6
Dralagen <dralagen@dralagen.fr>
2015-03-12 22:39:53 +0100

    use popen for ls cmd, revert exemple.pdf

--- server.c
+++ server.c
@@ -1,9 +1,7 @@
 /*----------------------------------------------
   Serveur à lancer avant le client
   ------------------------------------------------*/
-
-
-#define DIR_DL "./" 
+#define DIR_DL "/tmp/FTP_server"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -24,47 +22,41 @@ typedef struct servent servent;
 
 
 /**
-	fonction d'envoie de la sortie d'une commande system
-*/
+  fonction d'envoie de la sortie d'une commande system
+  */
 
 // a voir pour l'histoire d'un chemin fictif pour le pwd ou seulement dans les sous dossier du serveur.
 void send_cmd(char * cmd,int socket){
 
- 		printf("commande :  %s\n",cmd);
-		FILE* cmdF;
-	    char cmd_c[100]="";
-		char cmd_t[100]="";
-		char contents[300]="";
+    printf("commande :  %s\n",cmd);
+    FILE* cmdF;
+    char cmd_c[100]="";
+    char cmd_t[100]="";
+    char contents[300]="";
 
-		strcat(cmd_t,cmd);
+    strcat(cmd_t,cmd);
 
-		//certain \n se retrouvait dans le code => suppression de ce symbole.
-		delete_retC(cmd_t);
+    //certain \n se retrouvait dans le code => suppression de ce symbole.
+    delete_retC(cmd_t);
 
-	    sprintf(cmd_c,"%s > /tmp/out.txt",cmd_t);
-		printf("%s \n",cmd_c);
-		system(cmd_c);
-
-		cmdF= fopen("/tmp/out.txt","r");
-
-		char tmp[200]="";
+    cmdF = popen(cmd_t, "r");
+    char tmp[200]="";
 
 
-		if(cmdF!=NULL){
-			while(fgets(contents,300, cmdF) != NULL){
-				strcat(tmp,contents);
-			}
+    if(cmdF!=NULL){
+        while(fgets(contents,300, cmdF) != NULL){
+            strcat(tmp,contents);
+        }
 
-		 strcpy(contents,tmp);
+        strcpy(contents,tmp);
 
-		}else{
-			fclose(cmdF);
-			perror("commande interrompu");
-		}
-	 	fclose(cmdF);
+    }else{
+        perror("commande interrompu");
+    }
+    pclose(cmdF);
 
 
-  	send_string(socket,contents);
+    send_string(socket,contents);
 
 }
 
@@ -72,14 +64,14 @@ void send_cmd(char * cmd,int socket){
 int main(int argc, char **argv) {
 
     int 	     	socket_descriptor_cmd, 		/* descripteur de socket */
-                nouv_socket_descriptor_cmd, 	/* [nouveau] descripteur de socket */
-                longueur_adresse_courante, /* longueur d'adresse courante d'un client */
-                action,
-                ack; // action courante demander par le client.
+                    nouv_socket_descriptor_cmd, 	/* [nouveau] descripteur de socket */
+                    longueur_adresse_courante, /* longueur d'adresse courante d'un client */
+                    action,
+                    ack; // action courante demander par le client.
 
 
     sockaddr_in 	adresse_locale, 		/* structure d'adresse locale*/
-                  adresse_client_courant; 	/* adresse client courant */
+                    adresse_client_courant; 	/* adresse client courant */
     hostent*		  ptr_hote; 			/* les infos recuperees sur la machine hote */
 
     char 		    machine[TAILLE_MAX_NOM+1]; 	/* nom de la machine locale */
@@ -94,7 +86,7 @@ int main(int argc, char **argv) {
 
 
     // create directory of file server
-    //mkdir(DIR_DL, 0777);
+    mkdir(DIR_DL, 1777);
 
     /* initialisation de la structure adresse_locale avec les infos recuperees */
 
@@ -114,10 +106,10 @@ int main(int argc, char **argv) {
        exit(1);
        }
        adresse_locale.sin_port = htons(ptr_service->s_port);
-    */
+       */
     /*-----------------------------------------------------------*/
     /* SOLUTION 2 : utiliser un nouveau numero de port */
-      adresse_locale.sin_port = htons(2000); // port de commande
+    adresse_locale.sin_port = htons(2000); // port de commande
     /*-----------------------------------------------------------*/
 
     printf("numero de port pour la connexion au serveur : %d \n",
@@ -156,116 +148,116 @@ int main(int argc, char **argv) {
         if (pid == 0) {
 
             //envoie a la connexion du client du chemin ou il se trouve.
-			char * pwd= "pwd";
+            char * pwd= "pwd";
             send_cmd("pwd",nouv_socket_descriptor_cmd);
             //ACK
             int actionR= recv(nouv_socket_descriptor_cmd,&action,sizeof(int),0);
             char * nomDeFichier;
-            char * toDl; 
+            char * toDl;
             while(action != EXIT){
 
                 if(action>0){ // si une action est envoyer par le client on la traite
 
-                        switch(action){
+                    switch(action){
 
-                            case UPLOAD:
-                                printf("action UPLOAD \n");
-                                int upOK=0;
-                                 
-                                int rupok = recv(nouv_socket_descriptor_cmd,&upOK,sizeof(int),0);
-                                printf("%d\n",upOK);
-                                printf("%d\n", rupok );
-                                if(rupok >0 && upOK){
-                                    //fichier sur le serveur ou déposer le fichier.
-                                    toDl = recv_string(nouv_socket_descriptor_cmd);
-                                    if(toDl != NULL ){
-                                               
-                                           if(reception_fichier(&nouv_socket_descriptor_cmd,toDl) == 1){
-                                                ack=1;
-                                                send(nouv_socket_descriptor_cmd,&ack,sizeof(int),0);
-                                            }
-                                    }else{
-                                        printf("erreur de reception du chemin ou sauvegarder le fichier");
+                        case UPLOAD:
+                            printf("action UPLOAD \n");
+                            int upOK=0;
+
+                            int rupok = recv(nouv_socket_descriptor_cmd,&upOK,sizeof(int),0);
+                            printf("%d\n",upOK);
+                            printf("%d\n", rupok );
+                            if(rupok >0 && upOK){
+                                //fichier sur le serveur ou déposer le fichier.
+                                toDl = recv_string(nouv_socket_descriptor_cmd);
+                                if(toDl != NULL ){
+
+                                    if(reception_fichier(&nouv_socket_descriptor_cmd,toDl) == 1){
+                                        ack=1;
+                                        send(nouv_socket_descriptor_cmd,&ack,sizeof(int),0);
                                     }
+                                }else{
+                                    printf("erreur de reception du chemin ou sauvegarder le fichier");
                                 }
-                                 
+                            }
+
                             break;
 
-                            case DOWLOAD:
-                                printf("action DOWNLAOD \n");
-                                //reception du nom de fichier a envoyer au client
-                                //todo vérification de la réception des éléments
-                                int downok=0;
-                                     
-                                nomDeFichier= recv_string(nouv_socket_descriptor_cmd);
-                                
-                                if(nomDeFichier != NULL){    //transfert du fichier demander;
-                                 if(file_exists(nomDeFichier)){
-                                     downok = 1;
-                                    
+                        case DOWLOAD:
+                            printf("action DOWNLAOD \n");
+                            //reception du nom de fichier a envoyer au client
+                            //todo vérification de la réception des éléments
+                            int downok=0;
+
+                            nomDeFichier= recv_string(nouv_socket_descriptor_cmd);
+
+                            if(nomDeFichier != NULL){    //transfert du fichier demander;
+                                if(file_exists(nomDeFichier)){
+                                    downok = 1;
+
                                     if(send(nouv_socket_descriptor_cmd,&downok,sizeof(int),0)>0){
-                                    
+
                                         transfert_fichier(nouv_socket_descriptor_cmd,nomDeFichier);
                                     }
-                                 }else { 
-                                    send(nouv_socket_descriptor_cmd,&downok,sizeof(int),0); 
-                                  }
-
+                                }else {
+                                    send(nouv_socket_descriptor_cmd,&downok,sizeof(int),0);
                                 }
 
+                            }
+
 
                             break;
 
-                            case LS_CMD:
-                                printf("action LS_CMD \n");
-                                int fok=0;
-                                int sfok=0;
-                                char * filePath=recv_string(nouv_socket_descriptor_cmd);
-                                delete_retC(filePath);
-                              
-                                if(file_exists(filePath)){
-                                    fok=1;
-                                    sfok=send(nouv_socket_descriptor_cmd,&fok,sizeof(int),0);
-                                    if(sfok > 0){
-                                        char * cmd=recv_string(nouv_socket_descriptor_cmd);
-                                        send_cmd(cmd,nouv_socket_descriptor_cmd);
-                                    }else{
+                        case LS_CMD:
+                            printf("action LS_CMD \n");
+                            int fok=0;
+                            int sfok=0;
+                            char * filePath=recv_string(nouv_socket_descriptor_cmd);
+                            delete_retC(filePath);
 
-                                        perror("erreur d'envoie de la vérification du fichier/dossier");
-                                    }
-                                }else{ // le dossier n'est pas disponible on renvoie 0;
-                                     
-                                    sfok=send(nouv_socket_descriptor_cmd,&fok,sizeof(int),0);
-                                    if(sfok < 0){
+                            if(file_exists(filePath)){
+                                fok=1;
+                                sfok=send(nouv_socket_descriptor_cmd,&fok,sizeof(int),0);
+                                if(sfok > 0){
+                                    char * cmd=recv_string(nouv_socket_descriptor_cmd);
+                                    send_cmd(cmd,nouv_socket_descriptor_cmd);
+                                }else{
 
-                                        perror("erreur d'envoie de la vérification du fichier/dossier");
-                                    }
+                                    perror("erreur d'envoie de la vérification du fichier/dossier");
                                 }
+                            }else{ // le dossier n'est pas disponible on renvoie 0;
+
+                                sfok=send(nouv_socket_descriptor_cmd,&fok,sizeof(int),0);
+                                if(sfok < 0){
+
+                                    perror("erreur d'envoie de la vérification du fichier/dossier");
+                                }
+                            }
 
 
                             break;
-                            case CD_CMD:
-                                printf("action CD_CMD \n");
-								//récupération d'une demande de vérification
-								char * path=recv_string(nouv_socket_descriptor_cmd);
-                                delete_retC(path);
-								int f_ok= file_exists(path);
-								printf("%d \n",f_ok);
-								send(nouv_socket_descriptor_cmd,&f_ok,sizeof(int),0);
+                        case CD_CMD:
+                            printf("action CD_CMD \n");
+                            //récupération d'une demande de vérification
+                            char * path=recv_string(nouv_socket_descriptor_cmd);
+                            delete_retC(path);
+                            int f_ok= file_exists(path);
+                            printf("%d \n",f_ok);
+                            send(nouv_socket_descriptor_cmd,&f_ok,sizeof(int),0);
                             break;
-                        }
+                    }
 
                     int ract=recv(nouv_socket_descriptor_cmd,&action,sizeof(int),0);
 
-                     if(ract == 0 || ract < 0){
-                         action = EXIT;
-                         printf("problème  ou déconnexion du client \n" );
-                         close(nouv_socket_descriptor_cmd);
-                     }
-                        
+                    if(ract == 0 || ract < 0){
+                        action = EXIT;
+                        printf("problème  ou déconnexion du client \n" );
+                        close(nouv_socket_descriptor_cmd);
+                    }
+
                 }
 
-               
+
 
             }
 
@@ -282,5 +274,6 @@ int main(int argc, char **argv) {
 
     }
 
- return 0;
+    return 0;
 }
+
